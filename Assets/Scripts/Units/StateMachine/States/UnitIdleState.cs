f782a92ad18abda3dd54c378b39ffc689bd08274
Boyan Evtushok <boyan.evtushok@greent.games>
2023-04-11 23:19:51 +0400

    separate unitDeathState; change fightState transition

--- Assets/Scripts/Units/StateMachine/States/UnitIdleState.cs
+++ Assets/Scripts/Units/StateMachine/States/UnitIdleState.cs
@@ -1,51 +1,7 @@
-﻿using DG.Tweening;
-using RTS.UI;
-using UnityEngine;
-
-namespace RTS
+﻿namespace RTS
 {
     public class UnitIdleState : State
     {
         
-    }
-    
-    public class UnitDeathState : State
-    {
-        readonly UnitView unitView;
-
-        public bool IsDeathing;
-        
-        public UnitDeathState(UnitView unitView)
-        {
-            this.unitView = unitView;
-        }
-        
-        Sequence sequence;
-        public override void Enter()
-        {
-            base.Enter();
-            unitView.BodyCollider.enabled = false;
-
-            var startColor = unitView.renderer.color;
-            sequence?.Kill();
-            sequence = DOTween.Sequence()
-                .Append(unitView.renderer.DOColor(Color.white, 0.1f))
-                .Append(unitView.renderer.DOColor(startColor, 0.1f))
-                .SetLoops(2)
-                .OnComplete(OnEnd);
-            IsDeathing = true;
-        }
-
-        void OnEnd()
-        {
-            IsDeathing = false;
-            unitView.Destroy();
-        }
-        
-        public override void Exit()
-        {
-            base.Exit();
-            sequence?.Kill();
-        }
     }
 }
--- Assets/Scripts/Units/StateMachine/UnitStateMachine.cs
+++ Assets/Scripts/Units/StateMachine/UnitStateMachine.cs
@@ -4,29 +4,31 @@ namespace RTS
 {
     public class UnitStateMachine : StateMachine
     {
-        UnitIdleState idleState;
-        UnitFightState fightState;
-        UnitSearchEnemyState moveState;
-        UnitDeathState deathState;
+        UnitIdleState idle;
+        UnitFightState fight;
+        UnitSearchEnemyState search;
+        UnitDeathState death;
 
         public UnitStateMachine(UnitView unitView)
         {
-            idleState = new UnitIdleState();
-            fightState = new UnitFightState(unitView);
-            moveState = new UnitSearchEnemyState(unitView);
-            deathState = new UnitDeathState(unitView);
+            idle = new UnitIdleState();
+            fight = new UnitFightState(unitView);
+            search = new UnitSearchEnemyState(unitView);
+            death = new UnitDeathState(unitView);
             
-            AddTransition(idleState, moveState, NoMovingNoTarget);
-            AddTransition(moveState, idleState, NoMovingNoTarget);
-            AddTransition(fightState, idleState, NoTarget);
-            AddTransition(fightState, idleState, NoInRangeTarget);
-
-            AddAnyTransition(deathState, IsDeath);
-            AddAnyTransition(fightState, CanDamageEnemy);
+            AddTransition(idle, search, NoMovingNoTarget);
+            AddTransition(search, fight, CanDamageEnemy);
+            
+            AddTransition(search, idle, NoMovingNoTarget);
+            AddTransition(fight, idle, NoTarget);
+            AddTransition(fight, idle, NoInRangeTarget);
+            
+            AddAnyTransition(death, IsDeath);
 
             bool IsDeath() => unitView.UnitHealthSystem.Health.Value <= 0;
+            bool IsTakeDamage() => unitView.UnitHealthSystem.Health.Value <= 0;
             bool NoTarget() => unitView.EnemyDetector.CurrentTarget == null;
-            bool NoMovingNoTarget() => !moveState.IsMoving && NoTarget();
+            bool NoMovingNoTarget() => !search.IsMoving && NoTarget();
             bool CanDamageEnemy() => HaveEnemyTarget() && !NoInRangeTarget(); 
             bool HaveEnemyTarget() => unitView.EnemyDetector.CurrentTarget != null;
             bool NoInRangeTarget()
@@ -40,7 +42,7 @@ namespace RTS
             };
             
 
-            SetState(idleState);
+            SetState(idle);
         }
     }
 }
