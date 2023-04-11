0a687813ec5ac6987362f93c26142d2357b78ee3
Boyan Evtushok <boyan.evtushok@greent.games>
2023-04-11 22:43:34 +0400

    scripts: utils

---
+++ Assets/Scripts/Utils/Console.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace RTS.Utils
+{
+    public static class Console
+    {
+        public static void Log(this object obj, string msg, LogType logType = LogType.None)
+        {
+            Debug.Log($"{logType} {obj.GetType().Name}: {msg}");
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/LogType.cs
@@ -0,0 +1,11 @@
+﻿namespace RTS.Utils
+{
+    public enum LogType
+    {
+        None = 0,
+        SelectFeature = 1,
+        MovementFeature = 2,
+        UnitFeature = 3,
+        UpdateFeature = 4,
+    }
+}
